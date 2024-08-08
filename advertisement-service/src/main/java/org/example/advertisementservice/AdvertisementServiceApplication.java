695118d456d973293b50310bb4dd5aba98366296
ogulcan <ogyuak4@gmail.com>
2024-08-08 22:46:08 +0300

    Update AdvertisementServiceApplication.java

--- advertisement-service/src/main/java/org/example/advertisementservice/AdvertisementServiceApplication.java
+++ advertisement-service/src/main/java/org/example/advertisementservice/AdvertisementServiceApplication.java
@@ -2,18 +2,15 @@ package org.example.advertisementservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class AdvertisementServiceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AdvertisementServiceApplication.class, args);
     }
